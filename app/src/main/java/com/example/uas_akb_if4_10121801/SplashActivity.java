400798a521dd7b3679f78bf7e72a7d1581e48ec3
username_github_anda <mochamadarival@gmail.com>
2023-08-12 23:41:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uas_akb_if4_10121801/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.uas_akb_if4_10121801;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("CATAT ID");
+        }
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
